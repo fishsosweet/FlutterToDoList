d268c87cfa5d4c72b06db652d665602256d88346
fish <macarada77@gmail.com>
2025-03-11 14:16:15 +0700

    Day2 - Calendar

--- lib/Widget/Footer.dart
+++ lib/Widget/Footer.dart
@@ -1,15 +1,28 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class Footer extends StatelessWidget {
+class Footer extends StatefulWidget {
   final Function Addnotes;
-  
-  Footer({super.key, required this.Addnotes});
-  TextEditingController noteFooter = TextEditingController();
+
+  const Footer({super.key, required this.Addnotes});
+  @override
+  State<Footer> createState() => _FooterState();
+}
+
+class _FooterState extends State<Footer> {
+  var today = DateTime.now();
+  late TextEditingController noteFooter;
+
+  @override
+  void initState() {
+    super.initState();
+    noteFooter = TextEditingController();
+  }
 
   void _Click() {
-    Addnotes(noteFooter.text);
+    widget.Addnotes(noteFooter.text);
     noteFooter.text = "";
   }
 
@@ -32,7 +45,7 @@ class Footer extends StatelessWidget {
                   ),
                   color: const Color.fromARGB(255, 210, 180, 217),
                 ),
-                height: 200,
+                height: 400,
                 width: 450,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -44,20 +57,64 @@ class Footer extends StatelessWidget {
                         labelText: 'Thêm ghi chú',
                       ),
                     ),
+                    ElevatedButton(
+                      onPressed: () {
+                        showDialog(
+                          useSafeArea: true,
+                          context: context,
+                          builder: (BuildContext context) {
+                            var timeDialog = today;
+                            return StatefulBuilder(
+                              builder: (content, setStateDialog) {
+                                return AlertDialog(
+                                  content: Center(
+                                    child: SizedBox(
+                                      width: double.maxFinite,
+                                      height: 400,
+                                      child: TableCalendar(
+                                        calendarStyle: CalendarStyle(
+                                          markerSizeScale: 0.5,
+                                        ),
+                                        focusedDay: timeDialog,
+                                        firstDay: DateTime.utc(2000, 1, 1),
+                                        lastDay: DateTime.utc(2100, 12, 31),
+                                        selectedDayPredicate:
+                                            (day) => isSameDay(timeDialog, day),
+                                        onDaySelected: (
+                                          selectedDay,
+                                          focusedDay,
+                                        ) {
+                                          setStateDialog(() {
+                                            timeDialog = selectedDay;
+                                            timeDialog = focusedDay;
+                                          });
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        );
+                      },
+                      child: Text('Thời gian'),
+                    ),
                     SizedBox(
                       width: 250,
                       height: 50,
-                      
+
                       child: ElevatedButton(
                         onPressed: _Click,
                         style: ElevatedButton.styleFrom(
-                        
                           backgroundColor: Color.fromARGB(224, 130, 3, 241),
                         ),
-                        child: Text("Lưu",
-                        style: TextStyle(
-                          color: const Color.fromARGB(255, 255, 255, 255)
-                        ),),
+                        child: Text(
+                          "Lưu",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                          ),
+                        ),
                       ),
                     ),
                   ],
